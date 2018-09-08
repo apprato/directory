8e17032299abc039091553c8c7dd5258fa74eb26
Stephen Goudie <sgoudie@gmail.com>
2018-09-08 18:09:21 +1000

    scroll to top of page, h1,h2,h3,h4,h5,h6 sizes,

--- imports/ui/pages/GetStarted.js
+++ imports/ui/pages/GetStarted.js
@@ -4,7 +4,7 @@ import { Row, Col, Button, Glyphicon, Jumbotron } from 'react-bootstrap';
 const Index = () => (
   <div className="GetStarted">
     <Jumbotron className="text-center">
-      <h2>Create your Health & Fitness Business Listing</h2>
+      <h1>Create your Health & Fitness Business Listing</h1>
       <p>Join the growing number of heatlh experts australia wide and<br />fill in our simple sign up and get your listing online now</p>
       <p><a className="btn btn-success" href="/signup/trainer" role="button">Get Started</a></p>
     </Jumbotron>
--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -6,7 +6,7 @@ import Clients from '../containers/Clients.js';
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
-      <h2>Submit & Find Health & Fitness</h2>
+      <h1>Submit & Find Health & Fitness</h1>
       <p>Search Australia's biggest Health and fitness directory of businesses and locations in Australia</p>
       <p><a className="btn btn-success" href="/getstarted" role="button">List your Business</a></p>
     </Jumbotron>
