241081f157d35463bb7ae20c757d9e3cf6fece51
Stephen Goudie <sgoudie@gmail.com>
2018-09-09 21:54:22 +1000

    Pager - change to a reasonable size for mobile view

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,87 +1,195 @@
 /* eslint-disable max-len */
 
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Meteor } from 'meteor/meteor';
-import App from '../../ui/layouts/App.js';
-import Documents from '../../ui/pages/Documents.js';
-import NewDocument from '../../ui/pages/NewDocument.js';
-import EditDocument from '../../ui/containers/EditDocument.js';
-import ViewDocument from '../../ui/containers/ViewDocument.js';
-import Index from '../../ui/pages/Index.js';
-import Login from '../../ui/pages/Login.js';
-import NotFound from '../../ui/pages/NotFound.js';
-import RecoverPassword from '../../ui/pages/RecoverPassword.js';
-import ResetPassword from '../../ui/pages/ResetPassword.js';
-import Signup from '../../ui/pages/Signup.js';
-import GetStarted from '../../ui/pages/GetStarted.js';
-import Clients from '../../ui/pages/Clients.js';
-import Trainers from '../../ui/containers/TrainersList.js';
-import TrainersArea from '../../ui/containers/TrainersAreaList.js';
-import NewTrainerExperience from '../../ui/pages/NewTrainerExperience.js';
-import EditTrainerExperience from '../../ui/containers/EditTrainerExperience.js';
-import NewTrainerProfile from '../../ui/pages/NewTrainerProfile.js';
-import EditTrainerProfile from '../../ui/containers/EditTrainerProfile.js';
-import ViewClient from '../../ui/containers/ViewClient.js';
-import ViewTrainer from '../../ui/containers/ViewTrainer.js';
-import NewClient from '../../ui/pages/NewClient.js';
-
+import React from "react";
+import { render } from "react-dom";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Meteor } from "meteor/meteor";
+import App from "../../ui/layouts/App.js";
+import Documents from "../../ui/pages/Documents.js";
+import NewDocument from "../../ui/pages/NewDocument.js";
+import EditDocument from "../../ui/containers/EditDocument.js";
+import ViewDocument from "../../ui/containers/ViewDocument.js";
+import Index from "../../ui/pages/Index.js";
+import Login from "../../ui/pages/Login.js";
+import NotFound from "../../ui/pages/NotFound.js";
+import RecoverPassword from "../../ui/pages/RecoverPassword.js";
+import ResetPassword from "../../ui/pages/ResetPassword.js";
+import Signup from "../../ui/pages/Signup.js";
+import GetStarted from "../../ui/pages/GetStarted.js";
+import Clients from "../../ui/pages/Clients.js";
+import Trainers from "../../ui/containers/TrainersList.js";
+import TrainersArea from "../../ui/containers/TrainersAreaList.js";
+import NewTrainerExperience from "../../ui/pages/NewTrainerExperience.js";
+import EditTrainerExperience from "../../ui/containers/EditTrainerExperience.js";
+import NewTrainerProfile from "../../ui/pages/NewTrainerProfile.js";
+import EditTrainerProfile from "../../ui/containers/EditTrainerProfile.js";
+import ViewClient from "../../ui/containers/ViewClient.js";
+import ViewTrainer from "../../ui/containers/ViewTrainer.js";
+import NewClient from "../../ui/pages/NewClient.js";
 
 const authenticate = (nextState, replace) => {
   if (!Meteor.loggingIn() && !Meteor.userId()) {
     replace({
-      pathname: '/login',
-      state: { nextPathname: nextState.location.pathname },
+      pathname: "/login",
+      state: { nextPathname: nextState.location.pathname }
     });
   }
 };
 
 Meteor.startup(() => {
   render(
-    <Router history={ browserHistory }>
-      <Route path="/" component={ App }>
-        <IndexRoute name="index" component={ Index } />
-        <Route name="documents" path="/documents" component={ Documents } onEnter={ authenticate } />
-        <Route name="newDocument" path="/documents/new" component={ NewDocument } onEnter={ authenticate } />
-        <Route name="editDocument" path="/documents/:_id/edit" component={ EditDocument } onEnter={ authenticate } />
-        <Route name="viewDocument" path="/documents/:_id" component={ ViewDocument } onEnter={ authenticate } />
-        <Route name="login" path="/login" component={ Login } />
-        <Route name="recover-password" path="/recover-password" component={ RecoverPassword } />
-        <Route name="reset-password" path="/reset-password/:token" component={ ResetPassword } />
-        <Route name="signup" path="/signup" component={ Signup } />
-        <Route name="signup-client" path="/signup/client" component={ Signup } />
-        <Route name="signup-trainer" path="/signup/trainer" component={ Signup } />
-        <Route name="clients" path="/clients" component={ Clients } />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute name="index" component={Index} />
+        <Route
+          name="documents"
+          path="/documents"
+          component={Documents}
+          onEnter={authenticate}
+        />
+        <Route
+          name="newDocument"
+          path="/documents/new"
+          component={NewDocument}
+          onEnter={authenticate}
+        />
+        <Route
+          name="editDocument"
+          path="/documents/:_id/edit"
+          component={EditDocument}
+          onEnter={authenticate}
+        />
+        <Route
+          name="viewDocument"
+          path="/documents/:_id"
+          component={ViewDocument}
+          onEnter={authenticate}
+        />
+        <Route name="login" path="/login" component={Login} />
+        <Route
+          name="recover-password"
+          path="/recover-password"
+          component={RecoverPassword}
+        />
+        <Route
+          name="reset-password"
+          path="/reset-password/:token"
+          component={ResetPassword}
+        />
+        <Route name="signup" path="/signup" component={Signup} />
+        <Route name="signup-client" path="/signup/client" component={Signup} />
+        <Route
+          name="signup-trainer"
+          path="/signup/trainer"
+          component={Signup}
+        />
+        <Route name="clients" path="/clients" component={Clients} />
 
-        <Route name="trainers" path="/directory" component={ Trainers } />
-        <Route name="trainers" path="/directory/category/:_category" component={ Trainers } />
-        <Route name="trainers" path="/directory/category/:_category/page/:_id" component={ Trainers } />
+        <Route name="trainers" path="/directory" component={Trainers} />
+        <Route
+          name="trainers"
+          path="/directory/category/:_category"
+          component={Trainers}
+        />
+        <Route
+          name="trainers"
+          path="/directory/category/:_category/page/:_id"
+          component={Trainers}
+        />
 
-        <Route name="trainers" path="/directory/state/:_state" component={ Trainers } />
-        <Route name="trainers" path="/directory/category/:_category/state/:_state" component={ Trainers } />
-        <Route name="trainers" path="/directory/category/:_category/state/:_state/page/:_id" component={ Trainers } />
+        <Route
+          name="trainers"
+          path="/directory/state/:_state"
+          component={Trainers}
+        />
+        <Route
+          name="trainers"
+          path="/directory/category/:_category/state/:_state"
+          component={Trainers}
+        />
+        <Route
+          name="trainers"
+          path="/directory/category/:_category/state/:_state/page/:_id"
+          component={Trainers}
+        />
 
-        <Route name="trainers" path="/directory/search/:_search" component={ Trainers } />
-        <Route name="trainers" path="/directory/search/:_search/page/:_id" component={ Trainers } />
-        <Route name="trainers" path="/directory/search/:_search/category/:_category" component={ Trainers } />
-        <Route name="trainers" path="/directory/search/:_search/category/:_category/page/:_id" component={ Trainers } />
-        <Route name="listTrainersPagination" path="/directory/page/:_id" component={ Trainers } />
+        <Route
+          name="trainers"
+          path="/directory/search/:_search"
+          component={Trainers}
+        />
+        <Route
+          name="trainers"
+          path="/directory/search/:_search/page/:_id"
+          component={Trainers}
+        />
+        <Route
+          name="trainers"
+          path="/directory/search/:_search/category/:_category"
+          component={Trainers}
+        />
+        <Route
+          name="trainers"
+          path="/directory/search/:_search/category/:_category/page/:_id"
+          component={Trainers}
+        />
+        <Route
+          name="listTrainersPagination"
+          path="/directory/page/:_id"
+          component={Trainers}
+        />
 
-        <Route name="listTrainersArea" path="/directory/area/:area" component={ TrainersArea } />
-        <Route name="listTrainersAreaPagination" path="/directory/area/:area/page/:_id" component={ TrainersArea } />
+        <Route
+          name="listTrainersArea"
+          path="/directory/area/:area"
+          component={TrainersArea}
+        />
+        <Route
+          name="listTrainersAreaPagination"
+          path="/directory/area/:area/page/:_id"
+          component={TrainersArea}
+        />
 
-        <Route name="viewTrainer" path="/directory/:_id" component={ ViewTrainer } />
-        <Route name="NewTrainerExperience" path="/trainer/new/experience" component={ NewTrainerExperience } onEnter={ authenticate } />
-        <Route name="EditTrainerExperience" path="/trainer/edit/experience" component={ EditTrainerExperience } onEnter={ authenticate } />
-        <Route name="NewTrainerProfile" path="/trainer/new/profile" component={ NewTrainerProfile } onEnter={ authenticate } />
-        <Route name="EditTrainerProfile" path="/trainer/edit/profile" component={ EditTrainerProfile } onEnter={ authenticate } />
-        <Route name="newClient" path="/clients/new" component={ NewClient } onEnter={ authenticate } />
-        <Route name="viewClient" path="/clients/:_id" component={ ViewClient } />
-        <Route name="getStarted" path="/getstarted" component={ GetStarted } />
-        <Route path="*" component={ NotFound } />
+        <Route
+          name="viewTrainer"
+          path="/directory/:_id"
+          component={ViewTrainer}
+        />
+        <Route
+          name="NewTrainerExperience"
+          path="/trainer/new/experience"
+          component={NewTrainerExperience}
+          onEnter={authenticate}
+        />
+        <Route
+          name="EditTrainerExperience"
+          path="/trainer/edit/experience"
+          component={EditTrainerExperience}
+          onEnter={authenticate}
+        />
+        <Route
+          name="NewTrainerProfile"
+          path="/trainer/new/profile"
+          component={NewTrainerProfile}
+          onEnter={authenticate}
+        />
+        <Route
+          name="EditTrainerProfile"
+          path="/trainer/edit/profile"
+          component={EditTrainerProfile}
+          onEnter={authenticate}
+        />
+        <Route
+          name="newClient"
+          path="/clients/new"
+          component={NewClient}
+          onEnter={authenticate}
+        />
+        <Route name="viewClient" path="/clients/:_id" component={ViewClient} />
+        <Route name="getStarted" path="/getstarted" component={GetStarted} />
+        <Route path="*" component={NotFound} />
       </Route>
     </Router>,
-    document.getElementById('react-root')
+    document.getElementById("react-root")
   );
 });
--- imports/ui/components/TrainersList.js
+++ imports/ui/components/TrainersList.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import {browserHistory, Router, Route, MenuItem  } from 'react-router';
-import {Alert, Row, Col, Panel, FormControl, Image, ButtonToolbar, Button} from 'react-bootstrap';
-import ReactPaginate from 'react-paginate';
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
+import React from "react";
+import { browserHistory, Router, Route, MenuItem } from "react-router";
+import {
+  Alert,
+  Row,
+  Col,
+  Panel,
+  FormControl,
+  Image,
+  ButtonToolbar,
+  Button
+} from "react-bootstrap";
+import ReactPaginate from "react-paginate";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
 
-
-const handleNavigation = (_id) => {
+const handleNavigation = _id => {
   browserHistory.push(`/directory/${_id}`);
-}
+};
 
-const handleNavigationPager = (selected, filterSearch, filterCategory,) => {
+const handleNavigationPager = (selected, filterSearch, filterCategory) => {
   if (filterCategory) {
-    browserHistory.push('/directory/category/' + filterCategory + '/page/' + selected);
+    browserHistory.push(
+      "/directory/category/" + filterCategory + "/page/" + selected
+    );
+  } else if (filterSearch) {
+    browserHistory.push(
+      "/directory/search/" + filterSearch + "/page/" + selected
+    );
+  } else {
+    browserHistory.push("/directory/page/" + selected);
   }
-  else if (filterSearch) {
-    browserHistory.push('/directory/search/' + filterSearch + '/page/' + selected);
-  }
-  else {
-    browserHistory.push('/directory/page/' + selected);
-  }
-}
+};
 
 class TrainersList extends React.Component {
-
   constructor(props) {
     super(props);
     this.handleSearchEnter = this.handleSearchEnter.bind(this);
     this.state = {
       searchTerm: null,
-      stateTerm: null,
+      stateTerm: null
     };
 
     this.handleSearch = this.handleSearch.bind(this);
@@ -37,170 +46,253 @@ class TrainersList extends React.Component {
 
   handleSearch(event) {
     const searchTerm = event.target.value;
-    this.setState({searchTerm});
+    this.setState({ searchTerm });
     this.props.searchQuery.set(searchTerm);
   }
 
   handleSearchEnter(event) {
-    if (event.key === 'Enter') {
-      browserHistory.push('/directory/search/' + event.target.value );
+    if (event.key === "Enter") {
+      browserHistory.push("/directory/search/" + event.target.value);
     }
   }
 
-
-  handleSearchClick(event) {
-
-  }
+  handleSearchClick(event) {}
 
   handlePageClick(event) {
     let selected = Number(event.selected + 1);
-    handleNavigationPager(selected, this.filterSearch, this.filterCategory)
+    handleNavigationPager(selected, this.filterSearch, this.filterCategory);
   }
 
-  handleStateChange (element) {
+  handleStateChange(element) {
     console.log(element.filterState);
-    if(element===null || element.value===undefined || element.value===false)
-      this.setState({stateTerm: null});
-    else
-      browserHistory.push('/directory/state/' + element.value);
+    if (
+      element === null ||
+      element.value === undefined ||
+      element.value === false
+    )
+      this.setState({ stateTerm: null });
+    else browserHistory.push("/directory/state/" + element.value);
   }
 
-  handleCategoryChange (element) {
-    if(element===null || element.value===undefined || element.value===false)
-      this.setState({categoryTerm: null});
-    else
-      browserHistory.push('/directory/category/' + element.value);
+  handleCategoryChange(element) {
+    if (
+      element === null ||
+      element.value === undefined ||
+      element.value === false
+    )
+      this.setState({ categoryTerm: null });
+    else browserHistory.push("/directory/category/" + element.value);
   }
-
 
   render() {
     window.scrollTo(0, 0);
-    const {trainers} = this.props;
+    const { trainers } = this.props;
     // Listing based switch
     const area = this.props.area;
-    const page = this.props.page
+    const page = this.props.page;
     var category = this.props.category;
     var search = this.props.search;
 
     if (area) {
-      var hrefBuilder = '/directory/area/' + area + '/page/' + this.props.skipCount;
+      var hrefBuilder =
+        "/directory/area/" + area + "/page/" + this.props.skipCount;
     }
 
     var stateSelectValues = [
-      { value: 'ACT',  label: 'Australian Capital Territory', clearableValue: false },
-      { value: 'NSW',  label: 'New South Wales',              clearableValue: false },
-      { value: 'NT',   label: 'Northern Territory',           clearableValue: false },
-      { value: 'QLD',  label: 'Queensland',                   clearableValue: false },
-      { value: 'SA',   label: 'South Australia',              clearableValue: false },
-      { value: 'TAS',  label: 'Tasmania',                     clearableValue: false },
-      { value: 'VIC',  label: 'Victoria',                     clearableValue: false },
-      { value: 'WA',   label: 'Western Australia',            clearableValue: false }
+      {
+        value: "ACT",
+        label: "Australian Capital Territory",
+        clearableValue: false
+      },
+      { value: "NSW", label: "New South Wales", clearableValue: false },
+      { value: "NT", label: "Northern Territory", clearableValue: false },
+      { value: "QLD", label: "Queensland", clearableValue: false },
+      { value: "SA", label: "South Australia", clearableValue: false },
+      { value: "TAS", label: "Tasmania", clearableValue: false },
+      { value: "VIC", label: "Victoria", clearableValue: false },
+      { value: "WA", label: "Western Australia", clearableValue: false }
     ];
 
     var categorySelectValues = [
-      { value: 'Health Fitness Centre',     label: 'Health Fitness Centre',     clearableValue: false },
-      { value: 'Personal Training',         label: 'Personal Training',         clearableValue: false },
-      { value: 'Martial Arts',              label: 'Martial Arts',              clearableValue: false },
-      { value: 'Wellbeing Centre',          label: 'Wellbeing Centre',          clearableValue: false },
-      { value: 'Yoga',                      label: 'Yoga',                      clearableValue: false },
-      { value: 'Exercise Equipment',        label: 'Exercise Equipment',        clearableValue: false },
-      { value: 'Massage Therapy',           label: 'Massage Therapy',           clearableValue: false },
-      { value: 'Holistic Health',           label: 'Holistic Health',           clearableValue: false },
-      { value: 'Corporate Health Fitness',  label: 'Corporate Health Fitness',  clearableValue: false },
-      { value: 'Pilates',                   label: 'Pilates',                   clearableValue: false },
-      { value: 'Nutritional Supplements',   label: 'Nutritional Supplements',   clearableValue: false },
-      { value: 'Life Coaching',             label: 'Life Coaching',             clearableValue: false },
-      { value: 'Weight Loss',               label: 'Weight Loss',               clearableValue: false },
-      { value: 'Employment and Careers',    label: 'Employment and Careers',    clearableValue: false },
-      { value: 'Group Health Fitness',      label: 'Group Health Fitness',      clearableValue: false }
+      {
+        value: "Health Fitness Centre",
+        label: "Health Fitness Centre",
+        clearableValue: false
+      },
+      {
+        value: "Personal Training",
+        label: "Personal Training",
+        clearableValue: false
+      },
+      { value: "Martial Arts", label: "Martial Arts", clearableValue: false },
+      {
+        value: "Wellbeing Centre",
+        label: "Wellbeing Centre",
+        clearableValue: false
+      },
+      { value: "Yoga", label: "Yoga", clearableValue: false },
+      {
+        value: "Exercise Equipment",
+        label: "Exercise Equipment",
+        clearableValue: false
+      },
+      {
+        value: "Massage Therapy",
+        label: "Massage Therapy",
+        clearableValue: false
+      },
+      {
+        value: "Holistic Health",
+        label: "Holistic Health",
+        clearableValue: false
+      },
+      {
+        value: "Corporate Health Fitness",
+        label: "Corporate Health Fitness",
+        clearableValue: false
+      },
+      { value: "Pilates", label: "Pilates", clearableValue: false },
+      {
+        value: "Nutritional Supplements",
+        label: "Nutritional Supplements",
+        clearableValue: false
+      },
+      { value: "Life Coaching", label: "Life Coaching", clearableValue: false },
+      { value: "Weight Loss", label: "Weight Loss", clearableValue: false },
+      {
+        value: "Employment and Careers",
+        label: "Employment and Careers",
+        clearableValue: false
+      },
+      {
+        value: "Group Health Fitness",
+        label: "Group Health Fitness",
+        clearableValue: false
+      }
     ];
 
-    var Select = require('react-select');
+    var Select = require("react-select");
 
-    return (<div className="Trainers">
-      <div className="TrainerSearch">
-        <Row>
-          <Col xs={ 12 } sm={ 4 }>
-            <i className="fa fa-search"/>
-            <FormControl
-              type="search"
-              onKeyPress={ this.handleSearchEnter}
-              onClick={ this.handleSearchClick }
-              placeholder="Search business name"
-              className="Search"
+    return (
+      <div className="Trainers">
+        <div className="TrainerSearch">
+          <Row>
+            <Col xs={12} sm={4}>
+              <i className="fa fa-search" />
+              <FormControl
+                type="search"
+                onKeyPress={this.handleSearchEnter}
+                onClick={this.handleSearchClick}
+                placeholder="Search business name"
+                className="Search"
+              />
+            </Col>
+            <Col xs={12} sm={4}>
+              <div>
+                <Select
+                  name="state"
+                  value={this.props.state}
+                  options={stateSelectValues}
+                  onChange={this.handleStateChange.bind(this)}
+                  filterSearch={this.props.search}
+                  filterCategory={this.props.category}
+                  filterState={this.props.state}
+                />
+              </div>
+            </Col>
+            <Col xs={12} sm={4}>
+              <div>
+                <Select
+                  name="category"
+                  value={this.props.category}
+                  options={categorySelectValues}
+                  onChange={this.handleCategoryChange.bind(this)}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div className="Trainers-list">
+          {trainers.length > 0 ? (
+            trainers.map(
+              ({
+                _id,
+                logo,
+                businessName,
+                overview,
+                category,
+                state,
+                suburb,
+                image
+              }) => (
+                <Panel>
+                  <Row>
+                    <Col xs={8} sm={10}>
+                      <a
+                        href={"/directory/" + _id}
+                        key={_id}
+                        onClick={() => handleNavigation(_id)}
+                      >
+                        <h2>{businessName}</h2>
+                      </a>
+                      <p>{category}</p>
+                      <p>{overview}</p>
+                    </Col>
+                    <Col xs={12} sm={2}>
+                      <p>
+                        {state} > {suburb}
+                      </p>
+                      {logo ? (
+                        <Image
+                          src={"/" + "logos" + "/" + logo}
+                          alt={businessName}
+                          responsive
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </Col>
+                  </Row>
+                </Panel>
+              )
+            )
+          ) : (
+            <Alert>
+              Sorry there are no listings found for '{this.state.searchTerm}
+              .'
+            </Alert>
+          )}
+          <Row>
+            <ReactPaginate
+              pageCount={this.props.pageCount}
+              pageRangeDisplayed={1}
+              marginPagesDisplayed={2}
+              previousLabel={"<"}
+              nextLabel={">"}
+              breakLabel={<a href="">...</a>}
+              breakClassName={"break-me"}
+              pageNum={this.props.currentPage - 1}
+              initialPage={this.props.currentPage - 1}
+              hrefBuilder={page => hrefBuilder + page}
+              onPageChange={this.handlePageClick}
+              containerClassName={"pagination"}
+              subContainerClassName={"pages pagination"}
+              activeClassName={"active"}
+              disableInitialCallback="false"
+              filterSearch={this.props.search}
+              filterCategory={this.props.category}
+              filterState={this.props.state}
             />
-          </Col>
-          <Col xs={ 12 } sm={ 4 }>
-            <div>
-              <Select
-                name="state"
-                value={this.props.state}
-                options={stateSelectValues}
-                onChange={this.handleStateChange.bind(this)}
-                filterSearch={this.props.search}
-                filterCategory={this.props.category}
-                filterState={this.props.state}
-              />
-            </div>
-          </Col>
-          <Col xs={ 12 } sm={ 4 }>
-            <div>
-              <Select
-                name="category"
-                value={this.props.category}
-                options={categorySelectValues}
-                onChange={this.handleCategoryChange.bind(this)}
-              />
-            </div>
-          </Col>
-        </Row>
+          </Row>
+        </div>
       </div>
-      <div className="Trainers-list">
-        { trainers.length > 0 ? trainers.map(({_id, logo, businessName, overview, category, state, suburb, image}) => (
-          <Panel>
-            <Row>
-              <Col xs={ 8 } sm={ 10 }>
-                <a href={ "/directory/" + _id } key={ _id } onClick={ () => handleNavigation(_id) }>
-                  <h2>{businessName}</h2>
-                </a>
-                <p>{ category }</p>
-                <p>{ overview }</p>
-              </Col>
-              <Col xs={ 12 } sm={ 2 }>
-                <p>{ state } > { suburb }</p>
-                {logo ? <Image src={ '/' + 'logos' + '/' + logo } alt={ businessName } responsive/> : ''}
-              </Col>
-            </Row>
-          </Panel>
-        )) : <Alert>Sorry there are no listings found for '{ this.state.searchTerm }.'</Alert> }
-        <Row>
-          <ReactPaginate
-            pageCount={this.props.pageCount}
-            pageRangeDisplayed={1}
-            marginPagesDisplayed={10}
-            previousLabel={"<"}
-            nextLabel={">"}
-            pageNum={this.props.currentPage - 1}
-            initialPage={this.props.currentPage - 1}
-            hrefBuilder={(page) => hrefBuilder + page }
-            onPageChange={this.handlePageClick}
-            containerClassName={"pagination"}
-            subContainerClassName={"pages pagination"}
-            activeClassName={"active"}
-            disableInitialCallback="false"
-            filterSearch={this.props.search}
-            filterCategory={this.props.category}
-            filterState={this.props.state}
-          />
-        </Row>
-      </div>
-    </div>);
+    );
   }
 }
 
 TrainersList.propTypes = {
   trainers: React.PropTypes.array,
-  searchQuery: React.PropTypes.object,
+  searchQuery: React.PropTypes.object
 };
 
 export default TrainersList;
